91bdb0b314dc1869fe34ae064534ae2f24e7f586
Otman LAHRECHE <laotman.47@gmail.com>
2022-04-24 23:01:57 +0100

    updates

--- threads.py
+++ threads.py
@@ -1,7 +1,11 @@
+import datetime
+import sqlite3
+
 from PyQt5.QtCore import QThread, pyqtSignal
 
 from database_operations import load_workers, add_worker, update_worker, delete_worker, load_garde_month, \
     add_garde_month, check_month, delete_garde_month
+from tools import get_workerId_by_name
 
 
 class ThreadLoadingApp(QThread):
@@ -225,4 +229,234 @@ class ThreadDeleteGardeMonth(QThread):
             self._signal.emit(i)
 
         self._signal_result.emit(True)
+
+
+class ThreadGuardSurv(QThread):
+    _signal = pyqtSignal(int)
+    _signal_result = pyqtSignal(list)
+
+    def __init__(self, service, num_days, month, year):
+        super(ThreadGuardSurv, self).__init__()
+        self.num_days = num_days
+        self.month = month
+        self.year = year
+        self.service = service
+
+        self.data = [("Jours", "Date", "De 08h:00 à 16h:00", "De 16h:00 à 08h:00")]
+
+    def __del__(self):
+        self.terminate()
+        self.wait()
+
+    def run(self):
+        connection = sqlite3.connect("database/sqlite.db")
+        cur = connection.cursor()
+
+        for row in range(self.num_days):
+
+            prog = row * 100 / self.num_days
+            day = row + 1
+            x = datetime.datetime(self.year, self.month, day)
+            m = ""
+            if x.strftime("%A") == "Saturday":
+                m = "Samedi"
+            elif x.strftime("%A") == "Sunday":
+                m = "Dimanche"
+            elif x.strftime("%A") == "Monday":
+                m = "Lundi"
+            elif x.strftime("%A") == "Tuesday":
+                m = "Mardi"
+            elif x.strftime("%A") == "Wednesday":
+                m = "Mercredi"
+            elif x.strftime("%A") == "Thursday":
+                m = "Jeudi"
+            elif x.strftime("%A") == "Friday":
+                m = "Vendredi"
+
+            if self.month / 10 >= 1:
+                if day / 10 >= 1:
+                    date_day = str(day) + "/" + str(self.month) + "/" + str(self.year)
+                else:
+                    date_day = str(0) + str(day) + "/" + str(self.month) + "/" + str(self.year)
+            else:
+                if day / 10 >= 1:
+                    date_day = str(day) + "/" + str(0) + str(self.month) + "/" + str(self.year)
+                else:
+                    date_day = str(0) + str(day) + "/" + str(0) + str(self.month) + "/" + str(self.year)
+
+            sql_q = 'SELECT health_worker.full_name FROM health_worker INNER JOIN guard ON health_worker.worker_id = guard.gardien_id where service=? and guard.periode =? and guard.d =? and guard.m =? and guard.y =?'
+            cur.execute(sql_q, ('urgence_surv', 'light', day, self.month, self.year))
+            results_light = cur.fetchall()
+
+            sql_q = 'SELECT health_worker.full_name FROM health_worker INNER JOIN guard ON health_worker.worker_id = guard.gardien_id where service=? and guard.periode =? and guard.d =? and guard.m =? and guard.y =?'
+            cur.execute(sql_q, ('urgence_surv', 'night', day, self.month, self.year))
+            results_night = cur.fetchall()
+
+            light = ""
+            night = ""
+
+            if results_light:
+                rl = results_light[0]
+                light = light + str(rl[0])
+
+            if results_night:
+                rn = results_night[0]
+                night = night + str(rn[0])
+
+            data_day = (m, date_day, light, night)
+
+            self.data.append(data_day)
+            self._signal.emit(int(prog))
+
+        connection.close()
+        self._signal_result.emit(self.data)
+
+
+class Thread_create_guard(QThread):
+    _signal_status = pyqtSignal(int)
+    _signal = pyqtSignal(bool)
+
+    def __init__(self, service, num_days, month, year, table):
+        super(Thread_create_guard, self).__init__()
+        self.num_days = num_days
+        self.month = month
+        self.year = year
+        self.table = table
+        self.service = service
+
+    def __del__(self):
+        self.terminate()
+        self.wait()
+
+    def run(self):
+        connection = sqlite3.connect("database/sqlite.db")
+        cur = connection.cursor()
+        for row in range(self.num_days):
+            day = row + 1
+            prog = row * 100 / self.num_days
+            sql_q = 'SELECT health_worker.full_name FROM health_worker INNER JOIN guard ON health_worker.worker_id = guard.gardien_id where service=? and guard.periode =? and guard.d =? and guard.m =? and guard.y =?'
+            cur.execute(sql_q, (self.service, 'light', day, self.month, self.year))
+            results_light = cur.fetchall()
+            check = self.table.cellWidget(row, 2)
+            med_name = check.chose.currentText()
+
+            check_2 = self.table.cellWidget(row, 3)
+            med_name_2 = check_2.chose.currentText()
+
+            if results_light:
+
+                rl = results_light[0]
+
+                if str(rl[0]) == med_name:
+                    print("do nothing")
+                elif str(rl[0]) != med_name and med_name != "":
+                    id1 = get_workerId_by_name(str(rl[0]), self.service)[0]
+                    id_new = get_workerId_by_name(med_name, self.service)[0]
+                    id1 = id1[0]
+                    id_new = id_new[0]
+                    sql_q_light = 'DELETE FROM guard WHERE guard.d=? and guard.m=? and guard.y=? and guard.periode =? and guard.gardien_id =?'
+                    cur.execute(sql_q_light, (day, self.month, self.year, 'light', id1))
+
+                    sql_q_light = 'INSERT INTO guard (d,m,y,periode,gardien_id) values (?,?,?,?,?)'
+                    cur.execute(sql_q_light, (day, self.month, self.year, 'light', id_new))
+
+                elif str(rl[0]) != med_name and med_name == "":
+
+                    id1 = get_workerId_by_name(str(rl[0]), self.service)[0]
+                    id1 = id1[0]
+                    sql_q_light = 'DELETE FROM guard WHERE guard.d=? and guard.m=? and guard.y=? and guard.periode =? and guard.gardien_id =?'
+                    cur.execute(sql_q_light, (day, self.month, self.year, 'light', id1))
+
+            elif med_name != "":
+                id_new = get_workerId_by_name(med_name, self.service)[0]
+                id_new = id_new[0]
+                sql_q_light = 'INSERT INTO guard (d,m,y,periode,gardien_id) values (?,?,?,?,?)'
+                cur.execute(sql_q_light, (day, self.month, self.year, 'light', id_new))
+
+            # guard shift night :
+
+            sql_q = 'SELECT health_worker.full_name FROM health_worker INNER JOIN guard ON health_worker.worker_id = guard.gardien_id where service=? and guard.periode =? and guard.d =? and guard.m =? and guard.y =?'
+            cur.execute(sql_q, (self.service, 'night', day, self.month, self.year))
+            results_night = cur.fetchall()
+            print(results_night)
+
+            if results_night:
+                rn = results_night[0]
+
+                if str(rn[0]) == med_name_2:
+                    print("do nothing")
+                elif str(rn[0]) != med_name_2 and med_name_2 != "":
+                    id1 = get_workerId_by_name(str(rn[0]), self.service)[0]
+                    id_new = get_workerId_by_name(med_name_2, self.service)[0]
+                    id1 = id1[0]
+                    id_new = id_new[0]
+                    sql_q_light = 'DELETE FROM guard WHERE guard.d=? and guard.m=? and guard.y=? and guard.periode =? and guard.gardien_id =?'
+                    cur.execute(sql_q_light, (day, self.month, self.year, 'night', id1))
+
+                    sql_q_light = 'INSERT INTO guard (d,m,y,periode,gardien_id) values (?,?,?,?,?)'
+                    cur.execute(sql_q_light, (day, self.month, self.year, 'night', id_new))
+
+                elif str(rn[0]) != med_name_2 and med_name_2 == "":
+
+                    id1 = get_workerId_by_name(str(rn[0]), self.service)[0]
+                    id1 = id1[0]
+                    sql_q_light = 'DELETE FROM guard WHERE guard.d=? and guard.m=? and guard.y=? and guard.periode =? and guard.gardien_id =?'
+                    cur.execute(sql_q_light, (day, self.month, self.year, 'night', id1))
+
+            elif med_name_2 != "":
+                id_new = get_workerId_by_name(med_name_2, self.service)[0]
+                id_new = id_new[0]
+                sql_q_light = 'INSERT INTO guard (d,m,y,periode,gardien_id) values (?,?,?,?,?)'
+                cur.execute(sql_q_light, (day, self.month, self.year, 'night', id_new))
+
+            connection.commit()
+            self._signal_status.emit(int(prog))
+
+        connection.close()
+        self._signal.emit(True)
+
+
+class Thread_load_guards(QThread):
+    _signal_status = pyqtSignal(int)
+    _signal = pyqtSignal(list)
+    _signal_finish = pyqtSignal(bool)
+
+    def __init__(self, service, num_days, month, year):
+        super(Thread_load_guards, self).__init__()
+        self.num_days = num_days
+        self.month = month
+        self.year = year
+        self.service = service
+
+    def __del__(self):
+        self.terminate()
+        self.wait()
+
+    def run(self):
+        connection = sqlite3.connect("database/sqlite.db")
+        cur = connection.cursor()
+
+        for row in range(self.num_days):
+            day = row + 1
+            prog = row * 100 / self.num_days
+
+
+            sql_q = 'SELECT health_worker.full_name FROM health_worker INNER JOIN guard ON health_worker.worker_id = guard.gardien_id where service=? and guard.periode =? and guard.d =? and guard.m =? and guard.y =?'
+            cur.execute(sql_q, (self.service, 'light', day, self.month, self.year))
+            results_light = cur.fetchall()
+
+            sql_q = 'SELECT health_worker.full_name FROM health_worker INNER JOIN guard ON health_worker.worker_id = guard.gardien_id where service=? and guard.periode =? and guard.d =? and guard.m =? and guard.y =?'
+            cur.execute(sql_q, (self.service, 'night', day, self.month, self.year))
+            results_night = cur.fetchall()
+
+            list = []
+            list.append(row)
+            list.append(results_light)
+            list.append(results_night)
+
+            self._signal.emit(list)
+            self._signal_status.emit(int(prog))
+
+        connection.close()
+        self._signal_finish.emit(True)
 
---
+++ tools.py
@@ -0,0 +1,357 @@
+import sqlite3
+
+
+
+from epsp_pdf import EpspPdf
+import os
+basedir = os.path.dirname(__file__)
+
+
+def get_workerId_by_name(name, service):
+    connection = sqlite3.connect("database/sqlite.db")
+    cur = connection.cursor()
+    sql_q = 'SELECT worker_id FROM health_worker where full_name=? and service=?'
+    cur.execute(sql_q, (name, service))
+    results = cur.fetchall()
+    connection.close()
+    return results
+
+
+def get_workerService_by_name(name):
+    connection = sqlite3.connect("database/sqlite.db")
+    cur = connection.cursor()
+    sql_q = 'SELECT service FROM health_worker where full_name=?'
+    cur.execute(sql_q, (name,))
+    results = cur.fetchall()
+    connection.close()
+    return results
+
+def get_workers_count(service):
+    connection = sqlite3.connect("database/sqlite.db")
+    cur = connection.cursor()
+    sql_q = 'SELECT count(*) FROM health_worker where service=?'
+    cur.execute(sql_q, (service,))
+    results = cur.fetchall()
+    connection.close()
+    return results
+
+
+def get_guard_months_count(service):
+    connection = sqlite3.connect("database/sqlite.db")
+    cur = connection.cursor()
+    sql_q = 'SELECT count(*) FROM guard_mounth where service=?'
+    cur.execute(sql_q, (service,))
+    results = cur.fetchall()
+    connection.close()
+    return results
+
+
+def get_consultation_months_count(service):
+    connection = sqlite3.connect("database/sqlite.db")
+    cur = connection.cursor()
+    sql_q = 'SELECT count(*) FROM consultaion_mounth where service=?'
+    cur.execute(sql_q, (service,))
+    results = cur.fetchall()
+    connection.close()
+    return results
+
+
+def create_garde_page(service, grd_cons, month, year, data, path):
+    pdf = EpspPdf()
+    pdf.alias_nb_pages()
+    pdf.add_page()
+    pdf.set_font("helvetica", size=12)
+    pdf.cell(0, 10, "Service de: "+service, 0, 0, markdown=True)
+    pdf.ln(10)
+
+    pdf.set_font("helvetica", "B", size=17)
+    pdf.cell(0, 10, "Planing de "+grd_cons, 1, 0, "C")
+    pdf.ln(8)
+    m = ""
+    if month == 1:
+        m = "janvier"
+    elif month == 2:
+        m = "février"
+    elif month == 3:
+        m = "mars"
+    elif month == 4:
+        m = "avril"
+    elif month == 5:
+        m = "mai"
+    elif month == 6:
+        m = "juin"
+    elif month == 7:
+        m = "juillet"
+    elif month == 8:
+        m = "août"
+    elif month == 9:
+        m = "septembre"
+    elif month == 10:
+        m = "octobre"
+    elif month == 11:
+        m = "novembre"
+    elif month == 12:
+        m = "décembre"
+    pdf.set_font("helvetica", size=12)
+    pdf.cell(0, 10, "Mois de "+m+"/"+str(year), 0, 0, "C")
+
+    pdf.ln(8)
+
+    pdf.set_font("Times", size=10)
+    line_height = pdf.font_size * 1.9
+    col_width = pdf.epw / 4
+    fill = False
+    for row in data:
+        for datum in row:
+            if datum == "Vendredi" or datum == "Samedi":
+                fill = True
+
+            if datum == "Jours" or datum == "Date" or datum == "De 08h:00 à 20h:00" or datum == "De 20h:00 à 08h:00" or datum == "De 08h:00 à 16h:00" or datum == "De 16h:00 à 20h:00" or datum == "De 16h:00 à 08h:00":
+                pdf.set_font("Times", "B", size=10)
+                pdf.multi_cell(col_width, line_height, datum, border=1, ln=3, max_line_height=pdf.font_size)
+            else:
+                if(datum == " "):
+                    pdf.set_fill_color(215, 215, 215)
+                    pdf.set_font("Times", size=10)
+                    pdf.multi_cell(col_width, line_height, datum, border=1, ln=3, max_line_height=pdf.font_size,
+                                   fill=True)
+                else:
+                    pdf.set_fill_color(224, 235, 255)
+                    pdf.set_font("Times", size=10)
+                    pdf.multi_cell(col_width, line_height, datum, border=1, ln=3, max_line_height=pdf.font_size,
+                                   fill=fill)
+
+
+        fill = False
+
+        pdf.ln(line_height)
+
+    pdf.ln(1)
+    pdf.set_right_margin(30)
+    pdf.set_left_margin(30)
+    pdf.cell(0, 10, "Chef service", 0, 0, "L")
+    pdf.cell(0, 10, "D.S.S", 0, 0, "R")
+
+    pdf.output(path)
+
+
+def create_recap_page(service, rec, month, year, data, chef, path):
+    pdf = EpspPdf()
+    pdf.alias_nb_pages()
+    pdf.add_page()
+    pdf.set_font("helvetica", size=12)
+    pdf.cell(0, 10, "Service de: "+service, 0, 0, markdown=True)
+    pdf.ln(10)
+
+    pdf.set_font("helvetica", "B", size=17)
+    pdf.cell(0, 10, "RECAP de " + rec, 1, 0, "C")
+    pdf.ln(8)
+    m = ""
+    if month == 1:
+        m = "janvier"
+    elif month == 2:
+        m = "février"
+    elif month == 3:
+        m = "mars"
+    elif month == 4:
+        m = "avril"
+    elif month == 5:
+        m = "mai"
+    elif month == 6:
+        m = "juin"
+    elif month == 7:
+        m = "juillet"
+    elif month == 8:
+        m = "août"
+    elif month == 9:
+        m = "septembre"
+    elif month == 10:
+        m = "octobre"
+    elif month == 11:
+        m = "novembre"
+    elif month == 12:
+        m = "décembre"
+    pdf.set_font("helvetica", size=12)
+    pdf.cell(0, 10, "Mois de "+m+"/"+str(year), 0, 0, "C")
+
+    pdf.ln(8)
+
+    pdf.set_font("Times", size=10)
+    line_height = pdf.font_size * 1.9
+    col_width = pdf.epw / 5  # distribute content evenly
+    fill = False
+    for row in data:
+        for datum in row:
+            if datum == " / " or datum == "Jours ouvrable" or datum == "Jours week-end" or datum == "Jours fériés" or datum == "Total" :
+                pdf.set_font("Times", "B", size=10)
+                pdf.multi_cell(col_width, line_height, datum, border=1, ln=3, max_line_height=pdf.font_size)
+            else:
+                pdf.set_fill_color(224, 235, 255)
+                pdf.set_font("Times", size=10)
+
+                pdf.multi_cell(col_width, line_height, str(datum), border=1, ln=3, max_line_height=pdf.font_size,
+                                   fill=fill)
+
+        fill = not fill
+
+        pdf.ln(line_height)
+
+    pdf.ln(2)
+    pdf.set_right_margin(30)
+    pdf.set_left_margin(30)
+
+    if chef != "":
+        pdf.cell(0, 10, "Recuperation de chef service (4 jours): " + chef, 0, 0, "L")
+
+    pdf.output(path)
+
+
+def create_garde_inf_page(service, grd_cons, month, year, data, groupes, path):
+    pdf = EpspPdf()
+    pdf.alias_nb_pages()
+    pdf.add_page()
+    pdf.set_font("helvetica", size=12)
+    pdf.cell(0, 10, "Service de: "+service, 0, 0, markdown=True)
+    pdf.ln(10)
+
+    pdf.set_font("helvetica", "B", size=17)
+    pdf.cell(0, 10, "Planing de "+grd_cons, 1, 0, "C")
+    pdf.ln(8)
+    m = ""
+    if month == 1:
+        m = "janvier"
+    elif month == 2:
+        m = "février"
+    elif month == 3:
+        m = "mars"
+    elif month == 4:
+        m = "avril"
+    elif month == 5:
+        m = "mai"
+    elif month == 6:
+        m = "juin"
+    elif month == 7:
+        m = "juillet"
+    elif month == 8:
+        m = "août"
+    elif month == 9:
+        m = "septembre"
+    elif month == 10:
+        m = "octobre"
+    elif month == 11:
+        m = "novembre"
+    elif month == 12:
+        m = "décembre"
+    pdf.set_font("helvetica", size=12)
+    pdf.cell(0, 10, "Mois de "+m+"/"+str(year), 0, 0, "C")
+
+    pdf.ln(8)
+
+    pdf.set_font("Times", size=10)
+    line_height = pdf.font_size * 1.7
+    col_width = pdf.epw / 4
+    fill = False
+    for row in data:
+        for datum in row:
+            if datum == "Vendredi" or datum == "Samedi":
+                fill = True
+
+            if datum == "Jours" or datum == "Date" or datum == "De 08h:00 à 20h:00" or datum == "De 20h:00 à 08h:00" or datum == "De 08h:00 à 16h:00" or datum == "De 16h:00 à 20h:00" or datum == "De 16h:00 à 08h:00":
+                pdf.set_font("Times", "B", size=10)
+                pdf.multi_cell(col_width, line_height, datum, border=1, ln=3, max_line_height=pdf.font_size)
+            else:
+                if(datum == " "):
+                    pdf.set_fill_color(215, 215, 215)
+                    pdf.set_font("Times", size=10)
+                    pdf.multi_cell(col_width, line_height, datum, border=1, ln=3, max_line_height=pdf.font_size,
+                                   fill=True)
+                else:
+                    pdf.set_fill_color(224, 235, 255)
+                    pdf.set_font("Times", size=10)
+                    pdf.multi_cell(col_width, line_height, datum, border=1, ln=3, max_line_height=pdf.font_size,
+                                   fill=fill)
+
+
+        fill = False
+
+        pdf.ln(line_height)
+
+    for groupe in groupes:
+        pdf.cell(0, 10, groupe, 0, 0, "L")
+        pdf.ln(4)
+
+
+    pdf.ln(20)
+    pdf.set_right_margin(30)
+    pdf.set_left_margin(30)
+    pdf.cell(0, 10, "Chef service", 0, 0, "L")
+    pdf.cell(0, 10, "D.S.S", 0, 0, "R")
+
+    pdf.output(path)
+
+
+def create_statistique_page(month, year, data, path):
+    pdf = EpspPdf()
+    pdf.alias_nb_pages()
+    pdf.add_page()
+    pdf.set_font("helvetica", size=12)
+    pdf.cell(0, 10, "Service de: RADIOLOGIE", 0, 0, markdown=True)
+    pdf.ln(10)
+
+    pdf.set_font("helvetica", "B", size=17)
+    pdf.cell(0, 10, "Statistique de service RADIOLOGIE", 1, 0, "C")
+    pdf.ln(8)
+    m = ""
+    if month == 1:
+        m = "janvier"
+    elif month == 2:
+        m = "février"
+    elif month == 3:
+        m = "mars"
+    elif month == 4:
+        m = "avril"
+    elif month == 5:
+        m = "mai"
+    elif month == 6:
+        m = "juin"
+    elif month == 7:
+        m = "juillet"
+    elif month == 8:
+        m = "août"
+    elif month == 9:
+        m = "septembre"
+    elif month == 10:
+        m = "octobre"
+    elif month == 11:
+        m = "novembre"
+    elif month == 12:
+        m = "décembre"
+    pdf.set_font("helvetica", size=12)
+    pdf.cell(0, 10, "Mois de "+m+"/"+str(year), 0, 0, "C")
+
+    pdf.ln(8)
+
+    pdf.set_font("Times", size=10)
+    line_height = pdf.font_size * 1.9
+    col_width = pdf.epw / 5  # distribute content evenly
+    fill = False
+    for row in data:
+        for datum in row:
+            if datum == "Examen" or datum == "Homme" or datum == "Famme" or datum == "Enfant" or datum == "Total":
+                pdf.set_font("Times", "B", size=10)
+                pdf.multi_cell(col_width, line_height, datum, border=1, ln=3, max_line_height=pdf.font_size)
+            else:
+                pdf.set_fill_color(224, 235, 255)
+                pdf.set_font("Times", size=10)
+
+                pdf.multi_cell(col_width, line_height, str(datum), border=1, ln=3, max_line_height=pdf.font_size,
+                                   fill=fill)
+
+        fill = not fill
+
+        pdf.ln(line_height)
+
+    pdf.ln(2)
+    pdf.set_right_margin(30)
+    pdf.set_left_margin(30)
+    pdf.output(path)
